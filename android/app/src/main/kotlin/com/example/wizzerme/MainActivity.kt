e1e507904f3be8263a341dfc8c9fe3977ec4f788
Eranda Bandaranaike <e.bandaranaike@veridatefinancial.com>
2024-01-15 01:04:00 +0530

    First commit from Eranda's machine

---
+++ android/app/src/main/kotlin/com/example/wizzerme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wizzerme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(
+      items: List<ListItem>.generate(
+        10,
+        (i) {
+          return MessageItem('Sender $i', 'Message body $i');
+        },
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  final List<ListItem> items;
+
+  const MyApp({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Mixed List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView.builder(
+          // Let the ListView know how many items it needs to build.
+          itemCount: items.length,
+          // Provide a builder function. This is where the magic happens.
+          // Convert each item into a widget based on the type of item it is.
+          itemBuilder: (context, index) {
+            final item = items[index];
+
+            return ListTile(
+              leading: const Icon(Icons.flight),
+              title: item.buildTitle(context),
+              subtitle: item.buildSubtitle(context),
+              trailing: const Icon(Icons.more_vert),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+/// The base class for the different types of items the list can contain.
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+/// A ListItem that contains data to display a heading.
+class CompanyName implements ListItem {
+  final String heading;
+
+  CompanyName(this.heading);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.bodyMedium,
+    );
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
+}
+
+/// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(sender);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
+}
